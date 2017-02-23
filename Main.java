ffb3fce41afa2586abb4dfa0024bca7b7637ae8f
David Dean <ssjgokuy2k@gmail.com>
2017-02-23 12:27:25 -0800

    Added a simple test case

--- Main.java
+++ Main.java
@@ -1,91 +1,49 @@
+import java.io.IOException;
+
+
 /*
- * Raymond Schooley
- * David Dean
- * 02-20-2017
- * TCSS 342 - Project 2
+ * Project 2 - Main.
+ * Leah Ruisenor, Lorenzo Pacis, Alex Merk.
  */
-
-import java.io.*;
-import java.util.Scanner;
-
+/**
+ * Main driver for CodingTree.
+ * @author Leah Ruisenor, Lorenzo Pacis, Alex Merk.
+ * @version February 16, 2017.
+ */
 public class Main {
+	
+	private static final int BYTE_SIZE = 8;
+	private static final String FILE_NAME = "src/Input/WarAndPeace.txt";
+	private static final String CODE_OUTPUT_FILE_NAME = "src/Output/codes.txt";
+	private static final String COMP_FILE_NAME = "src/Output/compressed.txt";
 
-    private static final String TXT_FILE = "WarAndPeace.txt";
-    private static final String CODE_FILE = "codes.txt";
-    private static final String COMPRESS_FILE = "compressed.txt";
 
-    /** Main Method. */
-    public static void main(String[] args) throws IOException {
-        long uncompressed = 0;
-        long compressed = 0;
-        long ratio = 0;
-        long time = 0;
+	public static void main(final String[] args) throws IOException {
+		double myTimer = System.currentTimeMillis();
+	
+		
+		//the message to be encoded from the string builder.
+		String messageToCode = new ReadFile(FILE_NAME).readFile();
+		//constructs the coding tree with the message from the file.
+		CodingTree myHuffmanTree = new CodingTree(messageToCode);
+		
+		CodeFileOutput codeFile = new CodeFileOutput(CODE_OUTPUT_FILE_NAME, myHuffmanTree);
+		codeFile.writeCodeOutput();
+		CompressedFileOutput compress = new CompressedFileOutput(COMP_FILE_NAME, messageToCode, myHuffmanTree);
+		compress.writeCompressedFile();
+		
+		//the cost of the uncompressed message
+		double unCompressedCost = messageToCode.length()*BYTE_SIZE;
+		
 
-        // Read in from a text file.
-        String fileAsString = readFile(TXT_FILE);
-        CodingTree treeCode = new CodingTree(fileAsString);
-
-        // Write coded file.
-        writeCodedFile(CODE_FILE, treeCode);
-
-        // Write compressed file & Get time to compress.
-        time = writeCompressFile(COMPRESS_FILE, treeCode, fileAsString);
-
-        // Apply results to display info
-        uncompressed = getFileSize(TXT_FILE);
-        compressed = getFileSize(COMPRESS_FILE);
-        ratio = compressed * 100 / uncompressed;
-        displayInfo(uncompressed, compressed, ratio, time);
-    }
-
-    /** Display the info. */
-    private static void displayInfo(long myUncompressed, long myCompressed, long myRatio, long myRunningTime) {
-        System.out.println("Uncompressed file size: " + myUncompressed + " bytes");
-        System.out.println("Compressed file size: " + myCompressed + " bytes");
-        System.out.println("Compression ratio: " + myRatio + "%");
-        System.out.println("Running Time: " + myRunningTime + " milliseconds");
-    }
-
-    /** Get file size. */
-    private static long getFileSize(String theFile) {
-        File file = new File(theFile);
-        return file.length();
-    }
-
-    /** Read the txt file into a string. */
-    private static String readFile(String theFile) throws IOException {
-        File file = new File(theFile);
-
-        StringBuilder returnFile = new StringBuilder();
-
-        Scanner in = new Scanner(new FileReader(file));
-        while (in.hasNextLine()) {
-            returnFile.append(System.lineSeparator());
-            returnFile.append(in.nextLine());
-        }
-        in.close();
-
-        return returnFile.toString();
-    }
-
-    /** Write the coded file. */
-    private static void writeCodedFile(String theFile, CodingTree theTree) throws IOException {
-        FileWriter file = new FileWriter(theFile);
-        file.write(theTree.myCodes.toString());
-        file.close();
-    }
-
-    /** Write the compressed file. */
-    private static long writeCompressFile(String theFile, CodingTree theTree, String theText) throws IOException {
-        long startTime = System.currentTimeMillis();
-
-        FileOutputStream compressFile = new FileOutputStream(theFile);
-
-        // Stuff
-
-        compressFile.close();
-
-        long endTime = System.currentTimeMillis();
-        return endTime - startTime;
-    }
+		StringBuilder displayInfo = new StringBuilder();
+		unCompressedCost = unCompressedCost/BYTE_SIZE;
+		double compressionCost = compress.getCompressionCost()/BYTE_SIZE;
+		displayInfo.append("Size of uncompressed file in bytes: " + unCompressedCost +"\n");
+		displayInfo.append("Size of compressed file in bytes: " + compressionCost + "\n");
+		displayInfo.append("File Compression Ratio: " + compressionCost * 100 / unCompressedCost + "%\n");
+		displayInfo.append("Total running time in miliseconds: " + (System.currentTimeMillis() - myTimer));
+		System.out.println(displayInfo.toString());
+		
+	}
 }
